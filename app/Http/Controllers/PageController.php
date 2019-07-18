d447fa451c1c5e421074c0eabfed3d5bbf906ee4
Tom Bird <thomas.bird1984@gmail.com>
2019-07-18 19:14:09 -0400

    Changed how the bridal party is sorted

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,7 +7,7 @@ class PageController extends Controller {
     public function page_data($slug) {
         if($slug === 'home') {
             $bride_groom = User::whereRaw('type_id = ? OR type_id = ?', [1,2])->get();
-            $bridal_party = User::whereRaw('type_id = ? OR type_id = ?', [4,5])->orderBy('bridal_party_order', 'DESC')->get();
+            $bridal_party = User::whereRaw('type_id = ? OR type_id = ?', [4,5])->orderBy('bridal_party_order', 'ASC')->get();
 
             return response(json_encode([
                 'brideGroom' => $bride_groom,

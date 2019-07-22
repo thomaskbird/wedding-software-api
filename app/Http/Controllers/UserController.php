d98678cbe6ac2573f757c5b1065622108c5bfea2
Tom Bird <thomas.bird1984@gmail.com>
2019-07-22 16:45:17 -0400

    Adding a guest list route

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Models\User;
+
+class UserController extends Controller {
+    public function guest_list() {
+        $users = User::all();
+        return response(json_encode(
+            $users->toArray()
+        ));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,8 @@ route::get('page/{slug}', ['as' => 'page_data', 'uses' => 'PageController@page_d
 route::post('rsvp-respond', ['as' => 'rsvp_respond', 'uses' => 'RsvpController@rsvp_respond']);
 
 route::get('csv-import', ['as' => 'csv_import', 'uses' => 'ImportController@csv_import']);
+
+route::get('guest-list', ['as' => 'guest_list', 'uses' => 'UserController@guest_list']);
 /**
  * Protected routes
  * These routes utilize the apiToken middleware for authorization

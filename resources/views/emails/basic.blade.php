c90e947c99a4dcaae006f57ab66ee693a1c1d425
Tom Bird <thomas.bird1984@gmail.com>
2019-07-23 23:33:55 -0400

    Fixing bugs

--- resources/views/emails/basic.blade.php
+++ resources/views/emails/basic.blade.php
@@ -9,18 +9,18 @@
     </tr>
     <tr>
         <td>Phone:</td>
-        <td></td>
+        <td>{{ $phone }}</td>
     </tr>
     <tr>
         <td>Email:</td>
-        <td></td>
+        <td>{{ $email }}</td>
     </tr>
     <tr>
         <td>Subject:</td>
-        <td></td>
+        <td>{{ $subject }}</td>
     </tr>
     <tr>
         <td>Message:</td>
-        <td></td>
+        <td>{{ $message }}</td>
     </tr>
 </table>
